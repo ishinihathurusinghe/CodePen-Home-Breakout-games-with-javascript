41e273a04eb9c8acc960a0fe28a79123c11f61fb
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-07-11 13:15:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-var x = canvas.width / 2;
+var x = canvas.width / 2
 var y = canvas.height - 30; 
 
 var dx = 2;
